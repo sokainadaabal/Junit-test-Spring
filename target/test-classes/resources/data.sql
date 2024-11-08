81597451bf5cbbbbbd67ee47ca42e0e0a4973045
sokainadaabal <sokainadaabal@gmail.com>
2024-11-08 15:50:29 +0100

    finall projet in test with java spring

---
+++ target/test-classes/resources/data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE STUDENT(
+    id  INT PRIMARY KEY AUTO_INCREMENT,
+    age INT,
+    name VARCHAR(50),
+    password VARCHAR(50)
+);
+
+
+
+
+INSERT INTO STUDENT (id, age, name, password) VALUES (1, 15, 'sokaina', 'password'),(2, 15, 'issaika', 'password');
